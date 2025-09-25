593aefc8b0736b122bf55dd810ceeb4f27ea4bc6
Issa-barry <issabarry67@gmail.com>
2025-09-25 15:07:32 +0200

    maj

--- src/app/demo/components/landing/landing-routing.module.ts
+++ src/app/demo/components/landing/landing-routing.module.ts
@@ -8,9 +8,13 @@ import { HomeComponent } from './home/home.component';
         { path: '', component: LandingComponent,
              children: [
             { path: '', component: HomeComponent },
-            { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }
+            { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+            { path: 'cgu', loadChildren: () => import('./cgu/cgu.module').then(m => m.CguModule) },
+            { path: 'cgv', loadChildren: () => import('./cgv/cgv.module').then(m => m.CgvModule) },
+            { path: 'etape-transfert', loadChildren: () => import('./etape-transfert/etape-transfert.module').then(m => m.EtapeTransfertModule) },
 
         ]},
+        
     ])],
     exports: [RouterModule]
 })
