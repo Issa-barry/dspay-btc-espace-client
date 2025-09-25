8178e88078122de6c158826225239ca6bf8e161e
Issa-barry <issabarry67@gmail.com>
2025-09-25 16:02:13 +0200

    ruting footer

--- src/app/demo/components/landing/ui/landing-footer/landing-footer.component.ts
+++ src/app/demo/components/landing/ui/landing-footer/landing-footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'landing-footer',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './landing-footer.component.scss'
 })
 export class LandingFooterComponent {
+
+  constructor(
+    public router: Router
+  ) { }
 
 }
