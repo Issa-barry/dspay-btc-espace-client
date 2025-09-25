7ba1003b5f63f0301dffd4c517c3ce0c73997f21
Issa-barry <issabarry67@gmail.com>
2025-09-25 12:36:42 +0200

    routing landing v1

--- src/app/demo/components/landing/landing-routing.module.ts
+++ src/app/demo/components/landing/landing-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LandingComponent } from './landing.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     imports: [RouterModule.forChild([
-        { path: '', component: LandingComponent }
+        { path: '', component: LandingComponent,
+             children: [
+            { path: '', component: HomeComponent },
+            { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }
+
+        ]},
     ])],
     exports: [RouterModule]
 })
