be5545d5206fae758bcc8af48de0546e686e4417
Issa-barry <issabarry67@gmail.com>
2025-10-05 13:28:30 +0200

    maj

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,24 +1,48 @@
-import { defineConfig } from "cypress";
+// cypress.config.ts
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "angular",
-      bundler: "webpack",
-    },
-    specPattern: "**/*.cy.ts",
-  },
-  component: {
-    devServer: {
-      framework: "angular",
-      bundler: "webpack",
-    },
-    specPattern: "**/*.cy.ts",
-  },
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.cy.ts',
-    supportFile: 'cypress/support/e2e.ts'
+    supportFile: 'cypress/support/e2e.ts',
+    setupNodeEvents(on, config) {
+      // Tu peux ajouter des listeners d'événements ici si besoin
+      return config;
+    },
   },
-  
+  video: true,
+  screenshotOnRunFailure: true,
 });
+
+
+
+/*╭────────────────────────────────────────────────────────────╮
+     │ Config de base  │
+     ╰────────────────────────────────────────────────────────────╯ */
+
+
+// import { defineConfig } from "cypress";
+
+// export default defineConfig({
+//   component: {
+//     devServer: {
+//       framework: "angular",
+//       bundler: "webpack",
+//     },
+//     specPattern: "**/*.cy.ts",
+//   },
+//   component: {
+//     devServer: {
+//       framework: "angular",
+//       bundler: "webpack",
+//     },
+//     specPattern: "**/*.cy.ts",
+//   },
+//   e2e: {
+//     baseUrl: 'http://localhost:4200',
+//     specPattern: 'cypress/e2e/**/*.cy.ts',
+//     supportFile: 'cypress/support/e2e.ts'
+//   },
+  
+// });
--- src/app/demo/service/role/role.service.spec.ts
+++ src/app/demo/service/role/role.service.spec.ts
@@ -1,13 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RoleService } from './role.service';
 
-import { RolePermissionService } from './role.service';
-
-describe('RolePermissionService', () => {
-  let service: RolePermissionService;
+describe('RoleService', () => {
+  let service: RoleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RolePermissionService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // utile si le service utilise HttpClient
+      providers: [RoleService],
+    });
+    service = TestBed.inject(RoleService);
   });
 
   it('should be created', () => {
