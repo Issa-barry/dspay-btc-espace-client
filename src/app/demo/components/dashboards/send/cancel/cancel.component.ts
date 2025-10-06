89decfa1b45884205e55bb1d7e1580f6ab2506c5
Issa-barry <issabarry67@gmail.com>
2025-10-06 20:04:36 +0200

    correction url stripe

--- src/app/demo/components/dashboards/send/cancel/cancel.component.ts
+++ src/app/demo/components/dashboards/send/cancel/cancel.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cancel',
@@ -6,6 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './cancel.component.html',
   styleUrl: './cancel.component.scss'
 })
-export class CancelComponent {
+export class CancelComponent implements OnInit {
 
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const orderId = this.route.snapshot.queryParamMap.get('order_id');
+    const cancelToken = this.route.snapshot.queryParamMap.get('cancel_token');
+
+    if (orderId && cancelToken) {
+      this.http.get(`http://127.0.0.1:8000/api/payments/stripe/checkout-session/cancel`, {
+        params: { order_id: orderId, cancel_token: cancelToken }
+      }).subscribe({
+        next: (res) => console.log('Annulation enregistrée côté serveur', res),
+        error: (err) => console.error('Erreur d’annulation', err)
+      });
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/dashboard/send']);
+  }
 }
