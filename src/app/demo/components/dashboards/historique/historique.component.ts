6839c6abe126f0380d563b5941a2170bd1c4690a
Issa-barry <issabarry67@gmail.com>
2025-11-01 12:05:45 +0100

    maj

--- src/app/demo/components/dashboards/historique/historique.component.ts
+++ src/app/demo/components/dashboards/historique/historique.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Transfert } from 'src/app/demo/models/transfert';
+import { TransfertService } from 'src/app/demo/service/transfert/transfert.service';
 
 @Component({
   selector: 'app-historique',
@@ -6,61 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historique.component.scss']
 })
 export class HistoriqueComponent implements OnInit {
-  transferts: any[] = [];
+  transferts: Transfert[] = [];
+   selectedTransferts: Transfert[] = [];
+   transfertDialog: boolean = false;
+   deleteTransfertsDialog: boolean = false;
+   transfert!: Transfert;
+    loading = false;
+    skeletonRows = Array.from({ length: 5 }, () => ({}));
+ 
+   constructor(
+     private router: Router, 
+     private transfertService: TransfertService,
+     private messageService: MessageService,
+    ) {}
+ 
 
   ngOnInit() {
-    this.transferts = [
-      {
-        id: 1,
-        beneficiaire: 'Abdourahman DIALLO',
-        phone: '+224 622 25 70 40',
-        montant_recu: 5000000
-      },
-      {
-        id: 2,
-        beneficiaire: 'Adama Camara',
-        phone: '+224 611 21 43 50',
-        montant_recu: 2500000
-      },
-      {
-        id: 3,
-        beneficiaire: 'Aissatou Diallo',
-        phone: '+224 627 75 59 33',
-        montant_recu: 7500000
-      },
-      {
-        id: 4,
-        beneficiaire: 'Alpha Ousmane Barry',
-        phone: '+224 622 22 21 98',
-        montant_recu: 3200000
-      },
-      {
-        id: 5,
-        beneficiaire: 'Aminata DIALLO',
-        phone: '+224 621 09 07 88',
-        montant_recu: 1500000
-      },
-      {
-        id: 6,
-        beneficiaire: 'Bintou BARRY',
-        phone: '+224 626 78 90 12',
-        montant_recu: 4800000
-      },
-      {
-        id: 7,
-        beneficiaire: 'Mamadou Sow',
-        phone: '+224 627 89 01 23',
-        montant_recu: 6200000
-      },
-      {
-        id: 8,
-        beneficiaire: 'Fatoumata Conde',
-        phone: '+224 628 90 12 34',
-        montant_recu: 2900000
-      }
-    ];
+       this.getTransfertsByUser();
   }
 
+   /** Récupérer la liste des transferts */
+  getTransfertsByUser(): void {
+      this.loading = true;
+
+    this.transfertService.getByUserAuth().subscribe({
+      next: (response: any) => {
+        this.transferts = response;
+        this.loading = false;
+        console.log(this.transferts); 
+        
+      },
+      error: (err: any) => {
+        console.error('Erreur lors de la récupération des transferts:', err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erreur',
+          detail: "Impossible de charger les transferts.",
+        });
+        this.loading = false;
+      }
+    });
+  }
+
+  // *****
   getInitials(name: string): string {
     const words = name.split(' ');
     if (words.length >= 2) {
@@ -71,21 +63,21 @@ export class HistoriqueComponent implements OnInit {
 
   getAvatarColor(name: string): string {
     /**Multicolor avatar */
-    // const colors = [
-    //   '#E91E63', // Rose
-    //   '#2196F3', // Bleu
-    //   '#FF9800', // Orange
-    //   '#4CAF50', // Vert
-    //   '#9C27B0', // Violet
-    //   '#FF5722', // Rouge-orange
-    //   '#00BCD4', // Cyan
-    //   '#FFC107'  // Jaune-orange
-    // ];
+    const colors = [
+      '#E91E63', // Rose
+      '#2196F3', // Bleu
+      '#FF9800', // Orange
+      '#4CAF50', // Vert
+      '#9C27B0', // Violet
+      '#FF5722', // Rouge-orange
+      '#00BCD4', // Cyan
+      '#FFC107'  // Jaune-orange
+    ];
     
-    // const index = name.length % colors.length;
-    // return colors[index];
+    const index = name.length % colors.length;
+    return colors[index];
 
     /**couleur unique avatar */
-     return '#00BCD4';
+    //  return '#00BCD4';
   }
 }
